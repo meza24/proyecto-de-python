26394a9814b5de0c20fa9d74d81fd3bdd7587908
meza24 <axel.meza@ucr.ac.cr>
2023-11-14 23:01:54 -0600

    Se agregar manejos de excepciones.

--- inventario9.py
+++ inventario9.py
@@ -13,12 +13,33 @@ class Factura:
     vacio y se va llenando con el nombre del producto como clave y una lista con el precio y la cantidad como valor.
     '''
     def __init__(self):
+        # Se inicializa el diccionario de productos con la lista de productos como base de datos.
         self.productos = {
-            123: ["Leche", 2000],
-            1234: ["Pan", 2100],
-            12345: ["Huevo", 2500],
-            123456: ["Arroz", 3100]
-        }
+    123: ["Leche", 2000],
+    1234: ["Pan", 2100],
+    12345: ["Huevo", 2500],
+    123456: ["Arroz", 3100],
+    234: ["Azúcar", 1800],
+    345: ["Frijoles", 2200],
+    456: ["Aceite", 3200],
+    567: ["Pasta", 2400],
+    678: ["Harina", 2000],
+    789: ["Café", 3500],
+    890: ["Carne de res", 6000],
+    901: ["Pollo", 4500],
+    912: ["Pescado", 5500],
+    102: ["Plátanos", 1800],
+    213: ["Papas", 2300],
+    324: ["Cebolla", 2000],
+    435: ["Tomate", 2500],
+    546: ["Zanahorias", 2100],
+    657: ["Mantequilla", 2800],
+    768: ["Queso", 3500],
+    879: ["Yogur", 2200],
+    980: ["Jabón", 2300],
+    111: ["Detergente", 2700],
+    222: ["Papel higiénico", 3000]
+}
         self.carrito = {}
 
     '''
@@ -58,11 +79,16 @@ class Factura:
     nombre del producto, el precio unitario y la cantidad.
     '''
     def mostrar_carrito(self):
+        print('---------------------------')
         print("Carrito de compras:")
+        # Se recorre el diccionario de carrito y se imprime el nombre del producto, el precio unitario y la cantidad.
         for producto, valores in self.carrito.items():
+            # Se extrae el precio y la cantidad de la lista de valores. 
             precio = valores[0]
             cantidad = valores[1]
+            # Se imprime el nombre del producto, el precio unitario y la cantidad.
             print(f"Producto: {producto}, Precio unitario: {precio}, Cantidad: {cantidad}")
+            # Se utiliza la funcion sleep para realizar una cuenta regresiva de 1 segundo entre cada producto.
             sleep(1)
 
     '''
@@ -71,7 +97,8 @@ class Factura:
     '''    
     def calcular_total(self):
         total = sum(valores[0] * valores[1] for valores in self.carrito.values())
-        print(f"Total a pagar: {total}")
+        print('---------------------------')
+        print(f"Total a pagar: {total} colones.")
 
 '''
 Inicio del programa principal donde se crea un objeto de la clase Factura y se llama a los metodos de la clase
@@ -82,23 +109,31 @@ fecha = datetime.datetime.now()
 print("Fecha: ", fecha)
 print("Bienvenido a Supermercado MezaFresh")
 
-'''
-Pedimos al usuario el codigo de barras y buscamos el producto en el diccionario de productos. De estar en el diccionario
-se agrega al carrito de compras, de lo contario se pide el nombre y el precio del producto y se agrega al diccionario de
-productos y al carrito de compras. El ciclo se repite hasta que el usuario ingrese '0'
-'''
-codigo = input("Ingrese el código de barras del producto o '0' para terminar: ")
+# Se inicia un bucle para solicitar código de barras y cantidad hasta que se introduzca '0' para terminar (este es el menu). 
+while True:
+    # Se utiliza un bloque try-except para manejar errores al ingresar el código de barras y la cantidad.
+    try:
+        # Se solicita el código de barras y la cantidad.
+        codigo = input("Ingrese el código de barras del producto o '0' para terminar: ")
+        
+        # Si se ingresa '0', se rompe el bucle y termina la entrada de productos.
+        if codigo == '0':
+            break
+        
+        # Se convierte el código de barras a entero.
+        codigo = int(codigo)
+        # Se solicita la cantidad de productos.
+        cantidad = int(input("Ingrese la cantidad: "))
+        
+        # Llamada a la función para agregar el producto al carrito de la factura. 
+        mi_factura.agregar_al_carrito(codigo, cantidad)
 
-'''
-Aquí se llama al método agregar_al_carrito de la clase Factura para agregar los productos al carrito de compras y se
-pide al usuario el código de barras del producto o '0' para terminar el ciclo. Si el producto ya existe en el carrito
-se suma la cantidad, de lo contrario se agrega el producto al carrito de compras y se pide el nombre y el precio del
-producto para agregarlo al diccionario de productos y al carrito de compras.
-'''
-while codigo != '0':
-    cantidad = int(input("Ingrese la cantidad: "))
-    mi_factura.agregar_al_carrito(int(codigo), cantidad)
-    codigo = input("Ingrese el código de barras del producto o '0' para terminar: ")
+    # Manejo de errores al ingresar el código de barras y la cantidad.     
+    except ValueError:
+        print("El código de barras y la cantidad deben ser enteros.")
+        # Si hay un error al ingresar el código o cantidad, se solicita nuevamente el código.
+        # Esto permite al usuario corregir su entrada.
+        continue
 
 '''
 Instanciamos el objeto mi_factura de la clase Factura y llamamos a los metodos mostrar_carrito y calcular_total para
